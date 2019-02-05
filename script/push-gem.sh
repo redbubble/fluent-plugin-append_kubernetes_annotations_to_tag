0af9363abf6c2e868a55117451e747863c11d1cf
Kaitlyn Mesa <kaitlyn.mesa@redbubble.com>
2019-02-05 14:54:08 +1100

    Fixed formatting

--- script/push-gem.sh
+++ script/push-gem.sh
@@ -3,10 +3,10 @@ mkdir -p ~/.gem/
 
 if [ -z "${GEM_HOST_API_KEY}" ]
 then
-    echo "ERROR: GEM_HOST_API_KEY is not set"
-    exit 1
+	echo "ERROR: GEM_HOST_API_KEY is not set"
+	exit 1
 else
-    echo "--- \n:rubygems_api_key: ${GEM_HOST_API_KEY}" > ~/.gem/credentials
+	echo "--- \n:rubygems_api_key: ${GEM_HOST_API_KEY}" > ~/.gem/credentials
 	chmod 600 ~/.gem/credentials
 	timeout -k 10m 00s gem push fluent-plugin-append-kubernetes-annotations-to-tag-${VERSION}.gem
 fi
